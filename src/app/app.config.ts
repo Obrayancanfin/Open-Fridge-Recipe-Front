7bebc1162d2782a271fc077729449fbb50ad0238
Canfin Obrayan <104076031+Obrayancanfin@users.noreply.github.com>
2024-10-24 11:15:59 +0200

    login systhem

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { bearerInterceptor } from './utils/interceptors/bearer.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([bearerInterceptor]))
+    ]
 };
